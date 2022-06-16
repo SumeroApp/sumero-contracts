69ddbc195c4e7aad8a974340171ab24db59245f0
Suraj Kohli <kohlisuraj94@gmail.com>
2022-06-16 18:19:22 +0530

    updated contract code according to PR comments

--- deploy/005_deploy_clay_bonds.js
+++ deploy/005_deploy_clay_bonds.js
@@ -19,7 +19,7 @@ module.exports = async ({
     const ClayBondsDeployed = await deployments.deploy('ClayBonds', {
         from: deployer,
         gasLimit: 4000000,
-        args: [clayToken.address, web3.utils.toWei('200000', 'ether'), web3.utils.toWei('20000', 'ether')]
+        args: [clayToken.address, web3.utils.toWei('10', 'ether')]
     });
     console.log(colors.green("\nCLAY BONDS ADDRESS:", ClayBondsDeployed.address));
 
@@ -37,6 +37,9 @@ module.exports = async ({
     console.log(colors.green("APY_PERCENT: ", (await clayBonds.APY_PERCENT()).toString()));
     console.log(colors.green("BONUS_APY_PERCENT: ", (await clayBonds.BONUS_APY_PERCENT()).toString()));
     console.log(colors.green("BONDS_ISSUANCE_PERIOD: ", (await clayBonds.BONDS_ISSUANCE_PERIOD()).toString()));
+    console.log(colors.green("maximumBondRewards: ", (await clayBonds.maximumBondRewards()).toString()));
+    console.log(colors.green("totalBondDeposits: ", (await clayBonds.totalBondDeposits()).toString()));
+    console.log(colors.green("hasEnoughClayLiquidity: ", (await clayBonds.hasEnoughClayLiquidity()).toString()));
     const daysLeftToMaturationDate = (await clayBonds.getDaysLeftToMaturationDate()).toString();
     const rewardPercent = (await clayBonds.getRewardPercent(daysLeftToMaturationDate)).toString();
     const reward = (await clayBonds.getReward(web3.utils.toWei('1', 'ether'), rewardPercent)).toString();
@@ -48,11 +51,20 @@ module.exports = async ({
     console.log(colors.green("clay balance of ClayBonds Contract: ", (await clayToken.balanceOf(ClayBondsDeployed.address)).toString()));
 
     console.log(colors.blue("\nIssuing zClayBonds: ....."));
+    console.log(colors.blue("\nBond Issuance reverts incase of insufficient CLAY liquidity: ....."));
+    await expect(clayBonds.issue(web3.utils.toWei('1', 'ether'))).to.be.reverted;
+
+    console.log(colors.blue("Provide CLAY liquidity to bonds contract: ....."));
+    await clayToken.mint(ClayBondsDeployed.address, web3.utils.toWei('1.8', 'ether'))
+
+    console.log(colors.blue("\nIssuing zClayBonds: ....."));
     const issueTx = await clayBonds.issue(web3.utils.toWei('1', 'ether'));
     const issueReceipt = await issueTx.wait();
 
     console.log(colors.green("bonds balance of user: ", (await clayBonds.balanceOf(deployer)).toString()));
     console.log(colors.green("clay balance of ClayBonds Contract: ", (await clayToken.balanceOf(ClayBondsDeployed.address)).toString()));
+    console.log(colors.green("totalBondDeposits: ", (await clayBonds.totalBondDeposits()).toString()));
+    console.log(colors.green("hasEnoughClayLiquidity: ", (await clayBonds.hasEnoughClayLiquidity()).toString()));
 };
 
 module.exports.tags = ['ClayBonds'];
