41843731f11756cc0c8ce47ee08b99f94e9d5e62
Suraj Kohli <kohlisuraj94@gmail.com>
2022-07-27 20:27:02 +0530

    Asset Manager update for staking pool list

--- contracts/AssetManager.sol
+++ contracts/AssetManager.sol
@@ -4,19 +4,30 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract AssetManager is Ownable {
-    uint256 public totalAssets;
     enum AssetStatus {
         Closed,
         Paused,
         Open
     }
-    mapping(address => AssetStatus) public assetStatus;
-    mapping(uint256 => address) public idToVerifiedAsset;
 
-    function add(address _asset) external onlyOwner {
+    mapping(address => AssetStatus) public assetStatus;
+
+    uint256 public totalEmps;
+    mapping(uint256 => address) public idToVerifiedEmp;
+
+    uint256 public totalStakingPools;
+    mapping(uint256 => address) public idToVerifiedStakingPool;
+
+    function addEmp(address _asset) external onlyOwner {
         assetStatus[_asset] = AssetStatus.Open;
-        totalAssets = totalAssets + 1;
-        idToVerifiedAsset[totalAssets] = _asset;
+        totalEmps = totalEmps + 1;
+        idToVerifiedEmp[totalEmps] = _asset;
+    }
+
+    function addStakingPool(address _asset) external onlyOwner {
+        assetStatus[_asset] = AssetStatus.Open;
+        totalStakingPools = totalStakingPools + 1;
+        idToVerifiedStakingPool[totalStakingPools] = _asset;
     }
 
     function pause(address _asset) external onlyOwner {
