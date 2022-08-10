f0611b380cd6f82a8a1ba691492adeb39ff5a610
Suraj Kohli <kohlisuraj94@gmail.com>
2022-08-10 18:03:50 +0530

    comments explaining maths behind new emp mint

--- tasks/mint-emp.js
+++ tasks/mint-emp.js
@@ -1,6 +1,34 @@
 
 // npx hardhat mint-emp --emp-address <0xf29F8f53203D2E173710a53E7453b2E20e1F87B6> --emp-deployer-address <0x8D9656505A20C9562488bfa6EA0d1eF6B12966d7> --initial-token-ratio <30000>
 // npx hardhat mint-emp --emp-address 0xf29F8f53203D2E173710a53E7453b2E20e1F87B6 --emp-deployer-address 0x8D9656505A20C9562488bfa6EA0d1eF6B12966d7 --initial-token-ratio 30000
+
+/**
+ *  What all does the task need in order to do a successful initial mint?
+ *  - Price of the identifier i.e. ETHUSD http://18.219.111.187/prices.json
+ *  - Actual CR to use is max(rawGCR, position CR Ratio)
+ *      - rawGCR => 0 (this happens because totalTokensOutstanding is 0)
+ *      - position CR Ratio => collateralRequirement + 0.15 i.e. 1.25 + 0.15 => 1.40 => 140%
+ *  - minSponsorToken => 0.02 (20000/10^6(collateralDecimals))
+ *  - Provide Either one of these
+ *      - Optional Argument: Collateral provided
+ *      - Optional Argument: NumOfTokens provided
+ * 
+ *  - You provide $1000 collateral
+ *  - numOfTokens = collateralAmount / (price * CR ratio)
+ *  - numOfTokens = 1000 / (1700*1.40) => 0.42
+ * 
+ *  - You provide numOfTokens e.g. 10 tokens
+ *  - collateralNeeded = (price * CR ratio) * numOfTokens
+ * */
+
+/**
+ *  What all does the task need in order to do successive mint?
+ *  - Price of the identifier i.e. ETHUSD http://18.219.111.187/prices.json
+ *  - Actual CR to use is max(rawGCR, position CR Ratio)
+ *      - rawGCR => totalTokensOutstandingUint.isZero() ? ethers.BigNumber.from(0) : totalPositionCollateralUint.mul(cumulativeFeeMultiplier).div(totalTokensOutstandingUint);
+ *      - position CR Ratio => collateralRequirement + 15%
+ * */
+
 task("mint-emp", "Mint the EMP")
     .addParam("empAddress", "Emp Contract dddress")
     .addParam("empDeployerAddress", "Emp deployer address")
@@ -61,7 +89,7 @@ task("mint-emp", "Mint the EMP")
 
             const collateralAmountObject = { rawValue: ethers.utils.parseUnits(collateralAmount.toString(), syntheticDecimals) };
             const numTokensObject = { rawValue: ethers.utils.parseUnits(minSponsorTokens.toString(), syntheticDecimals) };
-            
+
             let mintEmpTx;
             try {
                 mintEmpTx = await empInstance.create(collateralAmountObject, numTokensObject);
@@ -88,7 +116,7 @@ task("mint-emp", "Mint the EMP")
             const txUrl = getTxUrl(deployments.network, mintEmpTx.hash);
             if (txUrl != null) {
                 console.log(txUrl);
-            } 
+            }
         }
     );
 
