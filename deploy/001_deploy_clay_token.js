9666471bd5748b5feaee5bb2168d5543c52e3995
Suraj Kohli <kohlisuraj94@gmail.com>
2022-08-31 00:41:36 +0530

    deploying ClayToken to goerli, updating deployment script to make it concise

--- deploy/001_deploy_clay_token.js
+++ deploy/001_deploy_clay_token.js
@@ -1,34 +1,14 @@
 /**
  * This script deploys the native CLAY token contract
  * 
- * @dev
- * Mints Clay tokens for testing
  */
-const { isLocalNetwork, isForkedNetwork } = require('../utils/helper');
-const colors = require('colors');
-
-// this function is injected with HRE
-module.exports = async ({
-    getNamedAccounts,
-    deployments,
-}) => {
-
-    console.log(colors.bold("\n==> Running 001_deploy_clay_token script"));
-
-    console.log(colors.blue("\nNetwork Status isLocalNetwork: ", isLocalNetwork(), " isForkedNetwork: ", isForkedNetwork()));
-
+const func = async function (hre) {
+    const { deployments, getNamedAccounts } = hre;
     const { deploy } = deployments;
+
     const { deployer } = await getNamedAccounts();
-    console.log(colors.green("\nDEPLOYER ADDRESS is:", deployer));
 
-    const ClayTokenDeployed = await deploy('ClayToken', {
-        from: deployer,
-        gasLimit: 4000000,
-        args: [],
-        skipIfAlreadyDeployed: true
-    });
-    console.log(colors.green("CLAY TOKEN ADDRESS:", ClayTokenDeployed.address));
-
+    await deploy("ClayToken", { from: deployer, log: true, skipIfAlreadyDeployed: true });
 };
-
-module.exports.tags = ['ClayToken'];
+module.exports = func;
+func.tags = ["ClayToken"];
