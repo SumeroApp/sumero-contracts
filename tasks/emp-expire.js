b66116b40216165058b4c4d2c7f75b3400bb588f
Rojhat Toptamus <rojhattoptamus@gmail.com>
2022-09-07 15:41:18 +0300

    adding new emp tasks

---
+++ tasks/emp-expire.js
@@ -0,0 +1,30 @@
+
+// npx hardhat emp-expire --address <address> --network <network-name>
+
+task("emp-expire", "Expires EMPs")
+    .addParam("address", "Address of EMP to expire")
+    .setAction(
+        async (args, deployments, network) => {
+            const { expect } = require('chai');
+            const { getTxUrl } = require('../utils/helper');
+            const colors = require('colors');
+
+            const EMP = await hre.ethers.getContractFactory("contracts/UMA/financial-templates/expiring-multiparty/ExpiringMultiParty.sol:ExpiringMultiParty");
+            const emp = await EMP.attach(args.address);
+            console.log(colors.blue("\n Expiring EMP: ....."));
+            try {
+                expect(await emp.contractState()).to.eq(0);
+                const expireEmpTx = await emp.expire()
+                await expireEmpTx.wait();
+                expect(await emp.contractState()).to.eq(1);
+                const txUrl = getTxUrl(deployments.network, expireEmpTx.hash);
+                if (txUrl != null) {
+                    console.log(colors.yellow("\n",txUrl));
+                }
+            } catch (error) {
+                console.log(colors.red("\n Expiring EMP failed: ....."));
+                console.log(error);
+            }
+
+        }
+    );
---
+++ tasks/emp-settle.js
@@ -0,0 +1,47 @@
+// npx hardhat settle-emp --emp-address <address> --synthAddress <address> --network <network-name>
+
+task("emp-settle", "Settles emps")
+    .addParam("empAddress", "Address of EMP contract")
+    .addParam("synthAddress", "Address of the synthetic token contract")
+    .setAction(
+        async (args, deployments, network) => {
+            const { deployer } = await hre.getNamedAccounts();
+            const { getTxUrl } = require('../utils/helper');
+            const colors = require('colors');
+
+            const EMP = await hre.ethers.getContractFactory("contracts/UMA/financial-templates/expiring-multiparty/ExpiringMultiParty.sol:ExpiringMultiParty");
+            const emp = await EMP.attach(args.empAddress);
+
+            const SYNTH = await hre.ethers.getContractFactory("contracts/UMA/common/implementation/ExpandedERC20.sol:ExpandedERC20");
+            const synth = await SYNTH.attach(args.synthAddress);
+
+            const synthBalance = synth.balanceOf(deployer)
+            console.log(colors.blue("\n Approving synths: ....."));
+            try {
+                const approveTX = await synth.approve(args.address, synthBalance)
+                const approveTxUrl = getTxUrl(deployments.network, approveTX.hash);
+                if (approveTxUrl != null) {
+                    console.log(colors.yellow("\n",approveTxUrl));
+                }
+                console.log("Approve completed...")
+            } catch (error) {
+                console.log("Approving synthetic tokens failed...");
+                console.log(error);
+            }
+
+            console.log(colors.blue("\n Settling EMPs: ....."));
+            try {
+                console.log("Contract State: " + await emp.contractState())
+                const expireEmpTx = await emp.settleExpired()
+                await expireEmpTx.wait();
+                const txUrl = getTxUrl(deployments.network, expireEmpTx.hash);
+                if (txUrl != null) {
+                    console.log(colors.yellow("\n",txUrl));
+                }
+            } catch (error) {
+                console.log(colors.red("\n Settling EMP failed: ....."));
+                console.log(error);
+            }
+
+        }
+    );
