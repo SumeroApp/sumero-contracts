2bf82a8f73d6bd493f722eff9780e1613c0f0b49
Kris O'Shea <minimallifter@protonmail.com>
2022-11-07 17:00:01 +0000

    Gas Optimizations for ClayToken.sol
    The following code reduces deployment costs by 700 gas. The previous code version resulted in the keccak operation being performed whenever the variable is used, increasing gas costs relative to just storing the output hash. Changing the state variables to immutable will only perform hashing on contract deployment which will save gas.

--- contracts/ClayToken.sol
+++ contracts/ClayToken.sol
@@ -5,10 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract ClayToken is ERC20, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    bytes32 public immutable MINTER_ROLE;
+    bytes32 public immutable BURNER_ROLE;
 
     constructor() ERC20("Clay Token", "CLAY") {
+        MINTER_ROLE = keccak256("MINTER_ROLE");
+        BURNER_ROLE = keccak256("BURNER_ROLE");
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(MINTER_ROLE, msg.sender);
         _setupRole(BURNER_ROLE, msg.sender);
