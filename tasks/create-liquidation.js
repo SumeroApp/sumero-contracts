96053d6f3077ce6cc60de27d0054b25ba1037735
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2023-02-14 13:18:15 +0530

    create liquidation task

---
+++ tasks/create-liquidation.js
@@ -0,0 +1,52 @@
+// npx hardhat create-liquidation --emp-address <address> --sponsor <address> --network <network-name>
+
+task("create-liquidation", "creates liquidation on sponsor's position")
+    .addParam("empAddress", "Address of EMP contract")
+    .addParam("sponsor", "Address of the sponsor")
+    .setAction(
+        async (args, hre) => {
+            const { getTxUrl } = require('../utils/helper');
+            const { run } = require('hardhat');
+            const colors = require('colors');
+
+            console.log(colors.bold("\n==> Running create-liquidation task..."));
+
+            const EMP = await hre.ethers.getContractFactory("contracts/UMA/financial-templates/expiring-multiparty/ExpiringMultiParty.sol:ExpiringMultiParty");
+            const emp = await EMP.attach(args.empAddress);
+            let sponsor = args.sponsor
+            let tokensOutstanding = await emp.totalTokensOutstanding();
+            const rawCollateral = await emp.totalPositionCollateral()
+            let positionTokensOutstanding = (await emp.positions(sponsor)).tokensOutstanding.rawValue;
+
+            let GCR = rawCollateral.mul(ethers.BigNumber.from(ethers.utils.parseEther("1"))).div(tokensOutstanding);
+            console.log('GCR: ' + GCR)
+            console.log('positionTokensOutstanding: ' + positionTokensOutstanding)
+
+
+            await run("erc20-approve",{
+                name: "ClayToken",
+                address: await emp.collateralCurrency(),
+                spender: args.empAddress,
+                amount: positionTokensOutstanding.toString(),
+            })
+
+            await run("erc20-approve",{
+                name: "ClayToken",
+                address: await emp.tokenCurrency(),
+                spender: args.empAddress,
+                amount: positionTokensOutstanding.toString(),
+            })
+
+            tx = await emp.createLiquidation(
+                sponsor,
+                { rawValue: "0" },
+                { rawValue: GCR.toString() },
+                { rawValue: positionTokensOutstanding.toString() },
+                '1968160845',
+            );
+            const txUrl = getTxUrl(hre.deployments.getNetworkName(), tx.hash);
+            if (txUrl != null) {
+                console.log(txUrl);
+            }
+        }
+    );
