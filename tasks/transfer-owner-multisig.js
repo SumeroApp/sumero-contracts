d7578bb854cc85ed8fecd039e065622704de8f85
Kris O'Shea <minimallifter@protonmail.com>
2023-02-27 18:40:05 +0000

    Update transfer-owner-multisig
    Updated the transfer-owner-multisig task by adding in a role param and converting the contract role into a roleHash in order for it to work. Also, added some additional console logs to make it very clear if it worked or not.

--- tasks/transfer-owner-multisig.js
+++ tasks/transfer-owner-multisig.js
@@ -1,33 +1,43 @@
-// npx hardhat transfer-owner-multisig --contract <address> --multisig <address> --network dashboard
+// npx hardhat transfer-owner-multisig --account <address> --role <role> --network dashboard
 
-// npx hardhat transfer-owner-multisig --multisig 0xD934fbEd3CB5dAa5A82C14089cAcaD6035718163 --contract 0xDA18Ac17C5789D94030a99E5347db2b0F2de355F --network dashboard
+// npx hardhat transfer-owner-multisig --account 0xD934fbEd3CB5dAa5A82C14089cAcaD6035718163 --role DEFAULT-ADMIN-ROLE --network dashboard
 
 task("transfer-owner-multisig", "Transfers ownership of contract to multisig address")
-.addParam("multisig", "The address of the multisig wallet")
-.addParam("contract", "The address of the deployed contract")
+.addParam("account", "The address of the new ownership wallet")
+.addParam("role", "The role to be assigned to the new wallet address")
 .setAction(
     async (args, hre) => {
+        const { expect } = require('chai');
         const { deployer } = await hre.getNamedAccounts();
+        const { ethers } = require("hardhat");
 
-        const Contract = await ethers.getContractFactory("contracts/ClayToken.sol:ClayToken");
-        const contract = await Contract.deploy();
+        const account = args.account
+        const Contract = await ethers.getContract("ClayToken", deployer);
+        console.log("Deployer Account: " + deployer)
+        console.log("Clay Contract Address: " + Contract.address);
+        //const contract = await Contract.deployed();
   
         // grant multisig DEFAULT_ADMIN_ROLE
-        const grantAdminTx = await contract.grantRole(DEFAULT_ADMIN_ROLE, multisig);
+        const roleHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(args.role));
+        const grantAdminTx = await Contract.grantRole(roleHash, (args.account));
         await grantAdminTx.wait();
-        const correctMultisigRole = await contract.hasRole(DEFAULT_ADMIN_ROLE, multisig);
+        const correctMultisigRole = await Contract.hasRole(roleHash, (args.account));
         expect(correctMultisigRole).to.be.true;
   
-        console.log(`ContractOwner: Grant DEFAULT_ADMIN_ROLE to multisig: ${multisig}`);
+        console.log(`ContractOwner: Grant MINTER_ROLE to multisig: ${account}`);
+        console.log("\nTransaction Receipt: \n", grantAdminTx);
   
          // revoke deployer DEFAULT_ADMIN_ROLE
-        const revokeAdminTx = await contract.renounceRole(DEFAULT_ADMIN_ROLE, deployer);
+        const revokeAdminTx = await Contract.renounceRole(roleHash, deployer);
         await revokeAdminTx.wait();
-        const correctDeployerRole = await contract.hasRole(DEFAULT_ADMIN_ROLE, deployer);
+        const correctDeployerRole = await Contract.hasRole(roleHash, deployer);
         expect(correctDeployerRole).to.be.false;
   
-        console.log(`ContractOwner: Revoke DEFAULT_ADMIN_ROLE from deployer: ${deployer}`);
+        console.log(`ContractOwner: Revoke MINTER_ROLE from deployer: ${deployer}`);
   
-        console.log("Transferred ownership to:", multisig);
+        console.log(`Transferred ownership to: ${account}`);
+
 }
 );
+
+module.exports = {};
