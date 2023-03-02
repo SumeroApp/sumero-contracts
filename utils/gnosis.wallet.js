d4de0e2e99e02784c6637ebe1a02b2bbd24b1d2e
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2023-03-02 19:11:08 +0530

    reject statement for wallet errors

--- utils/gnosis.wallet.js
+++ utils/gnosis.wallet.js
@@ -1,33 +1,37 @@
 const WalletConnectProvider = require("@walletconnect/web3-provider");
-const colors = require('colors'); 
+const colors = require('colors');
 
 const getWallet = () => new Promise(async (resolve, reject) => {
-    const goerliRpc = { 5: "https://eth-goerli.public.blastapi.io" }
-    const qrcode = false;
+    try {
+        const goerliRpc = { 5: "https://eth-goerli.public.blastapi.io" }
+        const qrcode = false;
 
-    const walletConnectProvider = new WalletConnectProvider.default({
-        rpc: {
-            ...goerliRpc
-            // ...
-        },
-          qrcode
-    });
+        const walletConnectProvider = new WalletConnectProvider.default({
+            rpc: {
+                ...goerliRpc
+            },
+            qrcode
+        });
 
-    walletConnectProvider.connector.on("display_uri", (err, payload) => {
-        const uri = payload.params[0];
-        console.log(colors.blue("\n Wallet connect URI: "+ uri));
-      });
+        walletConnectProvider.connector.on("display_uri", (err, payload) => {
+            const uri = payload.params[0];
+            console.log(colors.blue("\n Wallet connect URI: " + uri));
+        });
 
-    // Subscribe to session connection
-    walletConnectProvider.on("connect", () => {
-        const provider = new hre.ethers.providers.Web3Provider(
-            walletConnectProvider
-        );
-        console.log("connected to gnosis");
-        resolve(provider.getSigner());
-    });
+        // Subscribe to session connection
+        walletConnectProvider.on("connect", () => {
+            const provider = new hre.ethers.providers.Web3Provider(
+                walletConnectProvider
+            );
+            console.log("connected to gnosis");
+            resolve(provider.getSigner());
+        });
 
-    await walletConnectProvider.enable();
+        await walletConnectProvider.enable();
+    } catch (err) {
+        reject(err)
+    }
+
 })
 
 
