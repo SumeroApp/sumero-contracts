1491ea47bdfda8426631e2303163c89ca3e2de41
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2023-03-02 19:05:57 +0530

    gnosis wallet config

--- utils/gnosis.wallet.js
+++ utils/gnosis.wallet.js
@@ -1,99 +1,37 @@
-const { setupSafeDeployer } = require("hardhat-safe-deployer");
-const NodeWalletConnect = require("@walletconnect/node");
 const WalletConnectProvider = require("@walletconnect/web3-provider");
-const WalletConnectQRCodeModal = require("@walletconnect/qrcode-modal");
+const colors = require('colors'); 
 
-const getWallet = (ethers) => new Promise((resolve, reject) => {
+const getWallet = () => new Promise(async (resolve, reject) => {
+    const goerliRpc = { 5: "https://eth-goerli.public.blastapi.io" }
+    const qrcode = false;
 
-    // Create connector
-    const walletConnector = new NodeWalletConnect.default(
-        {
-            bridge: "https://bridge.walletconnect.org", // Required
-        },
-        {
-            clientMeta: {
-                description: "WalletConnect NodeJS Client",
-                url: "https://nodejs.org/en/",
-                icons: ["https://nodejs.org/static/images/logo.svg"],
-                name: "WalletConnect",
-            },
-        }
-    );
-
-
-    // Check if connection is already established
-    if (!walletConnector.connected) {
-        // create new session
-        walletConnector.createSession().then(() => {
-            // get uri for QR Code modal
-            const uri = walletConnector.uri;
-            // display QR Code modal
-            WalletConnectQRCodeModal.open(
-                uri,
-                () => {
-                    console.log("QR Code Modal closed");
-                },
-                true // isNode = true
-            );
-        });
-    }
-
-    // Subscribe to connection events
-    walletConnector.on("connect", async (error, payload) => {
-        if (error) {
-            reject(error);
-        }
-
-        console.log({
-            payload: payload.params[0].accounts
-        })
-
-        // Close QR Code Modal
-        WalletConnectQRCodeModal.close(
-            true // isNode = true
-        );
-
-        // Get provided accounts and chainId
-        const { accounts, chainId } = payload.params[0];
-
-        console.log({
-            walletConnector
-        })
-
-        //  Wrap with Web3Provider from ethers.js
-        const web3Provider = new ethers.providers.Web3Provider(walletConnector);
-
-        const signer = await web3Provider.getSigner()
-
-        resolve(signer)
-    });
-})
-
-const getWallet1 = (ethers) => new Promise(async (resolve, reject) => {
-    let bridge = "https://bridge.walletconnect.org";
-    let goerliRpc = {5: "https://eth-goerli.public.blastapi.io"}
-    let qrcode = false;
-    // let onUri: SimpleFunction | null = null;
-
-    const provider = new WalletConnectProvider.default({
+    const walletConnectProvider = new WalletConnectProvider.default({
         rpc: {
-          ...goerliRpc
-          // ...
+            ...goerliRpc
+            // ...
         },
-      }, qrcode);
+          qrcode
+    });
 
-      await provider.enable();
+    walletConnectProvider.connector.on("display_uri", (err, payload) => {
+        const uri = payload.params[0];
+        console.log(colors.blue("\n Wallet connect URI: "+ uri));
+      });
 
-      const web3Provider = new ethers.providers.Web3Provider(provider);
+    // Subscribe to session connection
+    walletConnectProvider.on("connect", () => {
+        const provider = new hre.ethers.providers.Web3Provider(
+            walletConnectProvider
+        );
+        console.log("connected to gnosis");
+        resolve(provider.getSigner());
+    });
 
-      const signer = await web3Provider.getSigner()
-
-      resolve(signer)
-  })
+    await walletConnectProvider.enable();
+})
 
 
 
 module.exports = {
     getWallet,
-    getWallet1
 }
