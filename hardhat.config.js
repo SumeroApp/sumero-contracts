07eaf5bd3364786f05d16790d04c57de07b04f83
Suraj Kohli <kohlisuraj94@gmail.com>
2023-03-06 15:05:49 +0530

    renaming tasks

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,9 +25,9 @@ require("./tasks/emp-request-withdrawal");
 require("./tasks/add-impl-to-finder");
 require("./tasks/create-lp");
 require("./tasks/setup-finder");
-require("./tasks/roles/transfer-clayToken-ownership");
-require("./tasks/roles/ownership-transfer");
-require("./tasks/transfer-ownerships");
+require("./tasks/transfer-clayToken-ownership");
+require("./tasks/transfer-contract-ownership");
+require("./tasks/transfer-all-ownerships");
 require("hardhat-gas-reporter");
 
 const solcVersion = "0.8.0";
--- tasks/transfer-all-ownerships.js
+++ tasks/transfer-all-ownerships.js
@@ -1,23 +1,23 @@
-// npx hardhat transfer-ownerships --emp <address> --sponsor <address> --network <network-name>
+// npx hardhat transfer-all-ownerships --emp <address> --sponsor <address> --network <network-name>
 
-task("transfer-ownerships", "Transfer all ownership/admin roles to target eth address")
+task("transfer-all-ownerships", "Transfer all ownership/admin roles to target eth address")
     .addParam("address", "Address of the intended new owner")
     .setAction(
         async (args, hre) => {
             const colors = require('colors');
             console.log(colors.bold("\n==> Transfer and renouncing all ownerships/admin roles to:" + args.address));
 
-            await run("ownership-transfer", {
+            await run("transfer-contract-ownership", {
                 contract: "AssetManager",
                 address: args.address,
             })
 
-            await run("ownership-transfer", {
+            await run("transfer-contract-ownership", {
                 contract: "ClayBonds",
                 address: args.address,
             })
 
-            await run("ownership-transfer", {
+            await run("transfer-contract-ownership", {
                 contract: "ClayStakingRewards",
                 address: args.address,
             })
