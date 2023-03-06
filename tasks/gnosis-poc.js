845403bb64ed3b7ac0d1f930af014937e5747002
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2023-03-06 16:02:52 +0530

    comments

--- tasks/gnosis-poc.js
+++ tasks/gnosis-poc.js
@@ -1,7 +1,7 @@
 task("gnosis-poc", "Approves ERC20 tokens to the given account")
     .setAction(async (args, hre) => {
 
-        const { Contract, Signer, providers } = require('ethers')
+        const { Contract, Signer, providers, Wallet } = require('ethers')
         const Safe = require('@safe-global/safe-core-sdk')
         const EthersAdapter = require('@safe-global/safe-ethers-lib')
         // const { ethers } = require('ethers')
@@ -10,16 +10,36 @@ task("gnosis-poc", "Approves ERC20 tokens to the given account")
 
         console.log("Setup provider")
         console.log("Setup SafeService")
-        const service = new SafeService(process.env.SERVICE_URL)
+        // const service = new SafeService(process.env.SERVICE_URL)
         console.log("Setup Signer")
         const [owner] = await hre.ethers.getSigners();
 
-        // const owner1 = owner.connect(new providers.JsonRpcProvider(process.env.ETH_NODE_URI_GOERLI))
+        //console.log("Setup provider")
+        const provider = new providers.JsonRpcProvider(process.env.JSON_RPC)
+        console.log("Setup SafeService")
+        const service = new SafeService(process.env.SERVICE_URL)
+        console.log("Setup Signer")
+        const signerOrProvider = new Wallet(process.env.SIGNER_KEY, provider)
 
         console.log("Setup SafeEthersSigner", process.env.DEPLOYER_SAFE)
+
+
+        /*
+        import { ethers } from "ethers"
+import Safe from "@safe-global/safe-core-sdk"
+import EthersAdapter from "@safe-global/safe-ethers-lib"
+
+const safe = await Safe.create({
+  ethAdapter: new EthersAdapter({ ethers, signerOrProvider }),
+  safeAddress: "some_safe_address"
+})
+        */
         const ethAdapter = new EthersAdapter.default({ ethers: hre.ethers, owner })
 
         const safe = await Safe.default.create({ ethAdapter, safeAddress: process.env.DEPLOYER_SAFE })
+
+
+
         const safeSigner = new SafeEthersSigner(safe, service, owner.provider)
         const contract = new Contract("0xe50c6391a6cb10f9B9Ef599aa1C68C82dD88Bd91", ["function pin(string newMessage)"], safeSigner)
         const proposedTx = await contract.functions.pin(`Local time: ${new Date().toLocaleString()}`)
