b3d45ba629d8d09038ee286a72d65b34fbdeaf48
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2023-03-14 20:29:01 +0530

    changes service url object name

--- gnosis/signer.js
+++ gnosis/signer.js
@@ -2,7 +2,7 @@ const Safe = require('@safe-global/safe-core-sdk')
 const { SafeEthersSigner, SafeService } = require('@safe-global/safe-ethers-adapters')
 const ethAdapter = require('./adapter')
 
-const serviceURLObj = {
+const chaidIdToServiceUrl = {
         1: 'https://safe-transaction-mainnet.safe.global',
         5: 'https://safe-transaction-goerli.safe.global',
     }
@@ -11,7 +11,7 @@ async function getGnosisSigner(deployerSafeAddress){
         if( !process.env.SERVICE_URL || process.env.SERVICE_URL == '') throw new Error("SERVICE_URL not defined/empty in .env")
         const chainId = await ethAdapter.getChainId()
         console.log("Initializing safe: ", deployerSafeAddress)
-        const service = new SafeService(serviceURLObj[chainId])
+        const service = new SafeService(chaidIdToServiceUrl[chainId])
         const safe = await Safe.default.create({ ethAdapter: ethAdapter, safeAddress: deployerSafeAddress })
         console.log("Safe created")
 
