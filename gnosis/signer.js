781757abeaa44119d7c4dabc1b1ee13049b0f264
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2023-03-15 14:23:59 +0530

    typo fix

--- gnosis/signer.js
+++ gnosis/signer.js
@@ -2,7 +2,7 @@ const Safe = require('@safe-global/safe-core-sdk')
 const { SafeEthersSigner, SafeService } = require('@safe-global/safe-ethers-adapters')
 const ethAdapter = require('./adapter')
 
-const chaidIdToServiceUrl = {
+const chainIdToServiceUrl = {
         1: 'https://safe-transaction-mainnet.safe.global',
         5: 'https://safe-transaction-goerli.safe.global',
     }
@@ -11,7 +11,7 @@ async function getGnosisSigner(deployerSafeAddress){
         if (!ethers.utils.isAddress(deployerSafeAddress)) throw new Error("Invalid safe address")
         const chainId = await ethAdapter.getChainId()
         console.log("Initializing safe: ", deployerSafeAddress)
-        const service = new SafeService(chaidIdToServiceUrl[chainId])
+        const service = new SafeService(chainIdToServiceUrl[chainId])
         const safe = await Safe.default.create({ ethAdapter: ethAdapter, safeAddress: deployerSafeAddress })
         console.log("Safe created")
 
