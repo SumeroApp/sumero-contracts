6d1e9c280bfc141e2dd38660aeb91416b43ce3df
Rojhat Toptamus <rojhattoptamus@gmail.com>
2023-03-17 10:53:04 +0300

    read addresses from excel file

--- tasks/build-merkle.js
+++ tasks/build-merkle.js
@@ -4,35 +4,55 @@ const keccak256 = require('keccak256');
 task("build-merkle", "builds merkle tree for clay distribution")
     .setAction(
         async (args, hre) => {
+            const colors = require('colors');
             const KECCAK256 = require('keccak256');
             const { MerkleTree } = require("merkletreejs");
-            const addressList = require('../utils/addressList.json');
-            const colors = require('colors');
+            const XLSX = require('xlsx');
 
-            let leaves = [];
-            console.log(colors.blue("\nBuilding Merkle Tree "));
+            const workbook = XLSX.readFile('../utils/airdrop.xlsx');
+            const sheetName = workbook.SheetNames[0];
+            console.log("Sheet Name:", sheetName);
+            const worksheet = workbook.Sheets[sheetName];
 
-            addressList.forEach(address => {
-                if (!ethers.utils.isAddress(address)) {
-                    throw "Improper address in the address list!"
+
+            // EXTRACTING THE VALUES
+            console.log(colors.blue("\nExtracting Addresses "));
+
+            const addresses = [];
+            const leaves = [];
+            const impropperAddresses = [];
+            let start = 5;
+            let end = 1503;
+            for (let i = start; i < end; i++) {
+                const address = worksheet[`H${i}`];
+                if (address !== undefined && address !== null) {
+                    if (!ethers.utils.isAddress(address.v)) {
+                        impropperAddresses.push(address.v);
+                    }
+                    else {
+                        addresses.push(address.v);
+                        leaves.push(keccak256(address.v))
+                    }
                 }
-            });
 
-            addressList.forEach(address => {
-                leaves.push(keccak256(address))
-            });
+            }
+            console.log(colors.green("\nExtracting Addresses Completed!"));
+            console.log(colors.red("\nImproper Addresses: ", impropperAddresses));
+
+
+            // BUILDING MERKLE TREE
+            console.log(colors.blue("\nBuilding Merkle Tree "));
 
             const tree = new MerkleTree(leaves, KECCAK256, { sortPairs: true });
             const root = tree.getHexRoot();
-
             console.log(colors.green("\nMerkle Tree Built "));
-            console.log(tree.toString());
             console.log("Merkle Root: " + root);
 
-            // test
-            /*          const leaf = KECCAK256('0xB9CcDD7Bedb7157798e10Ff06C7F10e0F37C6BdD');
-                        const proof = tree.getProof(leaf);
-                        console.log('proof: ', proof);
-                        console.log('Result:', tree.verify(proof, leaf, root)); */
+            // TESTING
+            console.log(colors.blue("\nTesting with a random address "));
+
+            const leaf = KECCAK256('0x356E25e6bF2D5f68684F5b758f644fbF02f6a782');
+            const proof = tree.getProof(leaf);
+            console.log('Is the address included in the leaves array:', tree.verify(proof, leaf, root));
         }
     );
