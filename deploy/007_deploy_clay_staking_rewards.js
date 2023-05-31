b26d4f5c2b64de18fc3b94ca0435a2e451e82f5c
Suraj Kohli <46536850+KohliSuraj@users.noreply.github.com>
2023-05-31 12:36:19 +0530

    Deployment Artifacts for user testing v0.1.5 (#112)
    * Deployment artifacts for dashboard-goerli-25thMay
    
    * use expiration timestamp instead of days while deploying clay staking rewards
    
    * updated EMPC verification task command in readme
    
    * deleting and regenerating pnpm lock file
    
    * update deploy script

--- deploy/007_deploy_clay_staking_rewards.js
+++ deploy/007_deploy_clay_staking_rewards.js
@@ -13,15 +13,14 @@ const func = async function (hre) {
 
     const ClayToken = await deployments.get("ClayToken");
 
-    let sumeroLpToken = { address: "0x8Ad9995145D24bA99326ea637218e1fc86C6b1fb" };
+    // change below address to point to correct LP token address
+    let sumeroLpToken = { address: "0x8556Fa1107401dAD95D67B1a9bbF49d87Fa18b4c" };
 
-    // change days depending on when you want staking to end
-    const days = 16;
-    const currentTimestamp = Date.now() / 1000;
-    const expirationTimestamp = Math.floor(currentTimestamp + (days * 24 * 3600));
+    // change timestamp depending on when you want staking to end
+    const expirationTimestamp = 1685368800;
+    // change max reward value for contract
     const maxReward = ethers.utils.parseEther("16000000").toString()
 
-    // hardcode above address to point to correct LP token address
     if (!sumeroLpToken.address) throw new Error("Need the LP token address");
 
     await deploy("ClayStakingRewards", {
