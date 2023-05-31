381f3b90abaebc18cebad68813c94012ffb46a0a
Suraj Kohli <kohlisuraj94@gmail.com>
2023-05-31 11:23:03 +0530

    update deploy script

--- deploy/007_deploy_clay_staking_rewards.js
+++ deploy/007_deploy_clay_staking_rewards.js
@@ -13,16 +13,14 @@ const func = async function (hre) {
 
     const ClayToken = await deployments.get("ClayToken");
 
+    // change below address to point to correct LP token address
     let sumeroLpToken = { address: "0x8556Fa1107401dAD95D67B1a9bbF49d87Fa18b4c" };
 
-    // change days depending on when you want staking to end
-    // const days = 4;
-    // const currentTimestamp = Date.now() / 1000;
-    // const expirationTimestamp = Math.floor(currentTimestamp + (days * 24 * 3600));
+    // change timestamp depending on when you want staking to end
     const expirationTimestamp = 1685368800;
+    // change max reward value for contract
     const maxReward = ethers.utils.parseEther("16000000").toString()
 
-    // hardcode above address to point to correct LP token address
     if (!sumeroLpToken.address) throw new Error("Need the LP token address");
 
     await deploy("ClayStakingRewards", {
